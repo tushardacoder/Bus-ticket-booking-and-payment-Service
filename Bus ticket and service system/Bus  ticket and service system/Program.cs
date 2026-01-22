636737e228ac4aabdf78c5d7d5118f1a8ce2457f
tushardacoder <tusharbasak071@gmail.com>
2026-01-22 20:28:01 +0600

    console dine

---
+++ Bus ticket and service system/Bus  ticket and service system/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("--- Bus Ticket Booking System ---- ");
+Console.WriteLine("1. Create User");
+Console.WriteLine("2. Show Users");
+Console.WriteLine("3. Create Bus");
+Console.WriteLine("4. Show Buses");
+Console.WriteLine("5. Create Schedule");
+Console.WriteLine("6. Show Schedules");
+Console.WriteLine("7. Show Schedule Details");
+Console.WriteLine("8. Book Ticket");
+Console.WriteLine("9. Show Invoices of a user");
+Console.WriteLine("10. Pay Invoice");
+Console.WriteLine("11. Show Tickets of a User");
+
+
+
+
